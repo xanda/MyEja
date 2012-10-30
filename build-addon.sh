6d8d8a52086d16a9be481ff2169ea66d14d0f7d7
xanda <adnan.shukor@gmail.com>
2012-10-30 11:49:25 +0800

    Addon 'builder'

---
+++ build-addon.sh
@@ -0,0 +1,21 @@
+BUILDnum=`head -n 1 dictionaries/ms-MY.dic`
+cat Mozilla-addon/install.rdf.orig | sed 's/\[build\_number\]/'$BUILDnum'/g' > Mozilla-addon/install.rdf
+
+cp dictionaries/ms-MY.* Mozilla-addon/dictionaries/
+cd Mozilla-addon
+
+whichZIP=`which zip`
+
+if [ "$whichZIP" != "/usr/bin/zip" ];
+then
+   echo "===============ERROR================"
+   echo " ERROR! zip is not installed"
+   echo "===============DONE=================" && exit 1
+fi
+
+zip ../MyEja.xpi dictionaries/* install.rdf icon.png
+rm dictionaries/*
+rm install.rdf
+
+echo ""
+echo "MyEja.xpi created!"
